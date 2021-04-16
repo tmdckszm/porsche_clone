a1451efc5faf23860ed2412cc99f9b414db76d5e
tmdckszm <78646428+tmdckszm@users.noreply.github.com>
2021-04-16 22:35:56 +0900

    Update porsche_main.js

--- porsche_main.js
+++ porsche_main.js
@@ -1,107 +1,158 @@
-var a718 = document.querySelector("#a718");
-var a911 = document.querySelector("#a911");
-var taycan = document.querySelector("#taycan");
-var panamera = document.querySelector("#panamera");
-var macan = document.querySelector("#macan");
-var cayenne = document.querySelector("#cayenne");
+var a718 =document.querySelector("#a718");
+var a911 =document.querySelector("#a911");
+var taycan =document.querySelector("#taycan");
+var panamera =document.querySelector("#panamera");
+var macan =document.querySelector("#macan");
+var cayenne =document.querySelector("#cayenne");
 
-var info = document.querySelector("#info");
-var info_img_1 = document.querySelector("#info_img_1");
-var info_display1 = document.querySelector("#info_display1");
-var info_img_2 = document.querySelector("#info_img_2");
-var info_display2 = document.querySelector("#info_display2");
-var info_img_3 = document.querySelector("#info_img_3");
-var info_display3 = document.querySelector("#info_display3");
-var info_img_4 = document.querySelector("#info_img_4");
-var info_display4 = document.querySelector("#info_display4");
-var info_img_5 = document.querySelector("#info_img_5");
-var info_display5 = document.querySelector("#info_display5");
-var info_img_6 = document.querySelector("#info_img_6");
-var info_display6 = document.querySelector("#info_display6");
-
-var a718f = false;
-var a911f = false;
-var taycanf = false;
-var panameraf = false;
-var macanf = false;
-var cayennef = false;
+var info1= document.querySelector(".info_menu_1");
+var info2= document.querySelector(".info_menu_2");
+var info3= document.querySelector(".info_menu_3");
+var info4= document.querySelector(".info_menu_4");
+var info5= document.querySelector(".info_menu_5");
+var info6= document.querySelector(".info_menu_6");
 
 a718.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_1.style.display = "block";
-    info_display1.style.display = "inline-block";
+    info1.style.display = "block";
+    info1.style.top = "0";
+    
+})
+
+a718.addEventListener("mouseout", function(){
+    info1.style.display = "none";
+    info1.style.top = "-500px";
+
+});
+
+info1.addEventListener("mouseover", function(){
+    info1.style.display = "block";
+    info1.style.top = "0";
+    
+})
+
+info1.addEventListener("mouseout", function(){
+    info1.style.display = "none";
+    info1.style.top = "-500px";
+
 });
 
 a911.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_2.style.display = "block";
-    info_display2.style.display = "inline-block";
+    info2.style.display = "block";
+    info2.style.top = "0";
+    
+})
+
+a911.addEventListener("mouseout", function(){
+    info2.style.display = "none";
+    info2.style.top = "-500px";
+
+});
+
+info2.addEventListener("mouseover", function(){
+    info2.style.display = "block";
+    info2.style.top = "0";
+    
+})
+
+info2.addEventListener("mouseout", function(){
+    info2.style.display = "none";
+    info2.style.top = "-500px";
 
 });
 
 taycan.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_3.style.display = "block";
-    info_display3.style.display = "inline-block";
+    info3.style.display = "block";
+    info3.style.top = "0";
     
+})
+
+taycan.addEventListener("mouseout", function(){
+    info3.style.display = "none";
+    info3.style.top = "-500px";
+
+});
+
+info3.addEventListener("mouseover", function(){
+    info3.style.display = "block";
+    info3.style.top = "0";
+    
+})
+
+info3.addEventListener("mouseout", function(){
+    info3.style.display = "none";
+    info3.style.top = "-500px";
+
+});
 
 panamera.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_4.style.display = "block";
-    info_display4.style.display = "inline-block";
+    info4.style.display = "block";
+    info4.style.top = "0";
+    
+})
+
+panamera.addEventListener("mouseout", function(){
+    info4.style.display = "none";
+    info4.style.top = "-500px";
+
+});
+
+info4.addEventListener("mouseover", function(){
+    info4.style.display = "block";
+    info4.style.top = "0";
+    
+})
+
+info4.addEventListener("mouseout", function(){
+    info4.style.display = "none";
+    info4.style.top = "-500px";
 
 });
 
 macan.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_5.style.display = "block";
-    info_display5.style.display = "inline-block";
-   
+    info5.style.display = "block";
+    info5.style.top = "0";
+    
+})
+
+macan.addEventListener("mouseout", function(){
+    info5.style.display = "none";
+    info5.style.top = "-500px";
+
+});
+
+info5.addEventListener("mouseover", function(){
+    info5.style.display = "block";
+    info5.style.top = "0";
+    
+})
+
+info5.addEventListener("mouseout", function(){
+    info5.style.display = "none";
+    info5.style.top = "-500px";
+
 });
 
 cayenne.addEventListener("mouseover", function(){
-    info.style.top = "0";
-    info_img_6.style.display = "block";
-    info_display6.style.display = "inline-block";
- 
-});
-
-
-a718.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_1.style.display = "none";
-    info_display1.style.display = "none";
+    info6.style.display = "block";
+    info6.style.top = "0";
     
-});
-
-a911.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_2.style.display = "none";
-    info_display2.style.display = "none";
-});
-
-taycan.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_3.style.display = "none";
-    info_display3.style.display = "none";
-});
-
-panamera.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_4.style.display = "none";
-    info_display4.style.display = "none";
-});
-
-macan.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_5.style.display = "none";
-    info_display5.style.display = "none";
-});
+})
 
 cayenne.addEventListener("mouseout", function(){
-    info.style.top = "-600px";
-    info_img_6.style.display = "none";
-    info_display6.style.display = "none";
+    info6.style.display = "none";
+    info6.style.top = "-500px";
+
 });
 
+info6.addEventListener("mouseover", function(){
+    info6.style.display = "block";
+    info6.style.top = "0";
+    
+})
+
+info6.addEventListener("mouseout", function(){
+    info6.style.display = "none";
+    info6.style.top = "-500px";
+
+});
 
